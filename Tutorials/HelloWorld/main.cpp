199694ecea56b119e69d5c18e625473fc3b0a7b8
woyaofacai <937907032@qq.com>
2015-11-06 11:57:19 -0500

    first tutorial

--- Tutorials/HelloWorld/main.cpp
+++ Tutorials/HelloWorld/main.cpp
@@ -28,7 +28,10 @@ int main()
 	groundMaterial.setAttribute("emissive", XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f));
 	groundMaterial.setPipeline("gf/geometry_dirlight");
 
+	// create ground's mesh
 	ISimpleMesh* groundMesh = meshManager->createPlaneMesh("ground", 100.0f, 100.0f, 10, 10);
+
+	// add a mesh node to the scene manager
 	IMeshNode* groundNode = smgr->addMeshNode(groundMesh, &groundMaterial, nullptr, true);
 
 	// create sphere's material
@@ -43,25 +46,26 @@ int main()
 	IMeshNode* sphereNode = smgr->addMeshNode(sphereMesh, &sphereMaterial, nullptr, true, XMFLOAT3(0, 1.0f, 0));
 
 	// add directional light
-	ILightNode* light = smgr->addDirectionalLight(1, nullptr, XMFLOAT3(1.0f, -1.0f, 1.0f));
+	ILightNode* light = smgr->addDirectionalLight(1, nullptr, XMFLOAT3(5.0f, -5.0f, 2.0f));
 	light->setSpecular(XMFLOAT4(1.0f, 1.0, 1.0f, 32.0f));
 	light->setDiffuse(XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f));
 
-	//ICameraNode* camera = smgr->addFpsCameraNode(1, nullptr, XMFLOAT3(0, 1.0f, -6.0f), XMFLOAT3(0, 1.0f, 0.0f), XMFLOAT3(0, 1.0f, 0), true);
-	ICameraNode* camera = smgr->addCameraNode(1, nullptr, XMFLOAT3(0, 1.0f, -6.0f), XMFLOAT3(0, 1.0f, 0.0f), XMFLOAT3(0, 1.0f, 0));
-
+	// set scene's environmental light 
 	smgr->setAmbient(XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f));
+
+	// create camera
+	ICameraNode* camera = smgr->addCameraNode(1, nullptr, XMFLOAT3(0, 1.0f, -6.0f), XMFLOAT3(0, 1.0f, 0.0f), XMFLOAT3(0, 1.0f, 0));
 
 	ITimer* timer = device->getTimer();
 	timer->reset();
 
-	const f32 color[] = { 1.0, 0, 0, 1.0f };
+	const f32 color[] = { 0, 0, 0, 1.0f };
 	while (device->run()) {
 		
 		float dt = timer->tick();
 		smgr->update(dt);
 		driver->beginScene(true, true, color);
-		//smgr->drawAll();
+		smgr->drawAll();
 		driver->endScene();
 	}
 
